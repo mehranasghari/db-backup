12a68513186062dd07487b74807955e4a41753a9
mehran <mhr.as1317@gmail.com>
2024-09-16 20:21:22 +0330

    add feature of backup from single db

--- backup.py
+++ backup.py
@@ -15,7 +15,27 @@ def backup_postgres_all_databases(host, port, user):
     with open(file_name, 'w') as file:
         try:
             subprocess.run(command, stdout=file, text=True, check=True)
-            print('Database backup complete')
+            print('Database backup completed')
+            print(f'Backup file saved in {file_name}')
+        except subprocess.CalledProcessError as e:
+            print(f'Error during pg_dumpall: {e.stderr}')
+
+def backup_postgres(host, port, user, database):
+    """Backup specified databases in PostgreSQL database using pg_dump"""
+    file_name = 'postgres_backup_' + database + '_' + datetime.now().strftime('%Y-%m-%d_%H%M%S') +'.db'
+    command = [
+            'pg_dump',
+            '-h', host,
+            '-p', port,
+            '-U', user,
+            '-d', database,
+            '-F', 'c',
+            '-w'
+        ]
+    with open(file_name, 'w') as file:
+        try:
+            subprocess.run(command, stdout=file, text=True, check=True)
+            print('Database backup completed')
             print(f'Backup file saved in {file_name}')
         except subprocess.CalledProcessError as e:
             print(f'Error during pg_dumpall: {e.stderr}')
--- cli.py
+++ cli.py
@@ -8,7 +8,7 @@ load_dotenv(dotenv_path='./db_secrets.env')
 
 @click.command()
 @click.option('-t', '--type', help='Database type', required=True, type=click.Choice(['postgres', 'mysql', 'mongodb'], case_sensitive=False))
-@click.option('-db', '--database', help='Database name to backup', default='all')
+@click.option('-d', '--database', help='Database name to backup', default='all')
 @click.option('-h', '--host', default=os.getenv('BACKUP_DB_HOST'),help='Database host', required=True, envvar='BACKUP_DB_HOST')
 @click.option('-p', '--port', default=os.getenv('BACKUP_DB_PORT'), help='Database port for connecting', required=True, envvar='BACKUP_DB_PORT')
 @click.option('-u', '--user', default=os.getenv('BACKUP_DB_USER'), help='With what user backup database', required=True, envvar='BACKUP_DB_USER')
@@ -17,13 +17,15 @@ def backup(type, database, host, port, user, password):
     if type == 'postgres':
         home_directory = os.path.expanduser("~")
         pgpass_file_path = os.path.join(home_directory, ".pgpass")
-        
+
         with open(pgpass_file_path, 'w') as pgpass_file:
             pgpass_file.write(f"{host}:{port}:*:{user}:{password}")
         os.chmod(pgpass_file_path, 0o600)
 
         if database == 'all':
             backup_postgres_all_databases(host, port, user)
+        else:
+            backup_postgres(host, port, user, database)
     
 if __name__ == '__main__':
     backup()
