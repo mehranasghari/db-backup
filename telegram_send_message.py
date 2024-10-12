e136660d32e0085b9e619d9bd293b3a40d8df074
mehran <mhr.as1317@gmail.com>
2024-10-12 23:02:54 +0330

    refactor telegram_send_message.py

--- telegram_send_message.py
+++ telegram_send_message.py
@@ -1,13 +1,15 @@
+from os import get_env
+from asyncio import run
 from telegram import Bot
-import asyncio
+''' import telegram library for sending status message to the telegram channel '''
 from dotenv import load_dotenv
-import os
 
 def send_message(message='default message'):
+    ''' get a message and send it to the telegram channel '''
     load_dotenv(dotenv_path='.env', override=True)
 
-    BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-    CHANNEL_ID = os.getenv('TELEGRAM_CHANNEL_ID')
-    bot = Bot(token=BOT_TOKEN)
+    bot_token = getenv('TELEGRAM_BOT_TOKEN')
+    channel_id = getenv('TELEGRAM_CHANNEL_ID')
+    bot = Bot(token=bot_token)
 
-    asyncio.run(bot.send_message(chat_id=CHANNEL_ID, text=message))
+    run(bot.send_message(chat_id=channel_id, text=message))
